99448116631c61ec99e520a557757f10f49134b7
louissailor <61705564+louissailor@users.noreply.github.com>
2020-12-21 20:08:49 +0300

    Initial commit

---
+++ include/parts/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+    <link rel="shortcut icon" href="assets/icons/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
